2d3bff09a21a7774f78c39f42c1b6edf052ff8fc
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2022-03-18 12:06:12 +0300

    refactor[bash.sh]: remove the file

--- bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum -y install httpd
-sudo systemctl enable httpd.service
-sudo systemctl start  httpd.service
-sudo aws s3 cp s3://dzmitry-karneyenka-s3-static-website /var/www/html/staticwebsite --recursive
